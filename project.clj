88f9a6047895f976d90d23772b50c3e4247ca0f8
sandric <kyrychenkoanton@gmail.com>
2014-07-26 16:00:19 +0300

    test version for separate computers worker

--- project.clj
+++ project.clj
@@ -9,4 +9,13 @@
   ;:git-dependencies [["https://github.com/clj-jgit/clj-jgit.git"]]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-jgit "0.7.2"]])
+                 [clj-jgit "0.7.2"]
+                 [org.jeromq/jeromq "0.3.0-SNAPSHOT"]
+                 [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]
+                 [org.clojure/data.json "0.2.5"]]
+
+
+  :repositories [ ["sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"]
+                  ["sonatype-snapshots"
+                    {:url "https://oss.sonatype.org/content/repositories/snapshots"
+                     :update :always}]])
--- src/chronicle/core.clj
+++ src/chronicle/core.clj
@@ -1,10 +1,14 @@
 (ns chronicle.core
   (:import (java.io FileNotFoundException)
            (org.eclipse.jgit.revwalk RevCommit)
-           (org.eclipse.jgit.api Git))
+           (org.eclipse.jgit.api Git)
+           (java.sql Time))
   (:require [chronicle.clj-jgit.internal]
             [chronicle.clj-jgit.porcelain]
-            [chronicle.clj-jgit.querying]))
+            [chronicle.clj-jgit.querying]
+            [zeromq.zmq :as zmq]
+
+            [clojure.data.json :as json]))
 
 (defn foo
   "I don't do a whole lot."
@@ -35,7 +39,30 @@
 
 (defn get_full_commits_history []
   (reverse
-    (map #(assoc %
+    (map #(dissoc (assoc %
            :diff (chronicle.clj-jgit.querying/changed-files-with-patch repo (:raw %))
            :files (get-commit-file-tree repo (:raw %)))
+           :repo :raw :time)
          (get_commits))))
+
+(defn worker []
+  (let [context (zmq/zcontext 1)]
+    (with-open [receiver (doto (zmq/socket context :pull)
+                           (zmq/connect "tcp://192.168.2.1:5557"))
+                sender (doto (zmq/socket context :push)
+                         (zmq/connect "tcp://192.168.2.1:5558"))]
+      (while (not (.. Thread currentThread isInterrupted))
+        (let [data (zmq/receive-str receiver)]
+          (println data "received query")
+
+          (get_repo "repos" (get (json/read-str data) "repo_name") (get (json/read-str data) "repo_url"))
+
+          (zmq/send-str sender (json/write-str {:message "there is some data I can't resist1..."}))
+          (zmq/send-str sender (json/write-str {:message "there is some data I can't resist2..."}))
+          (zmq/send-str sender (json/write-str {:message "there is some data I can't resist3..."}))
+
+          (zmq/send-str sender (json/write-str (get_full_commits_history)))
+          )))))
+
+(get_repo "repos", "slim", "https://github.com/slim-template/slim.git")
+(get_full_commits_history)
